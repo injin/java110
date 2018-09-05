dc03752a5b3cfc50a2ac6c6ddebc9851c82425de
lee <injin33@naver.com>
2018-09-05 10:39:26 +0900

    학생관리 기능추가

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -29,7 +29,32 @@ public class App {
         }
     }
     
-    static Member[] members = new Member[100];
+    static class Student extends Member {
+        protected String school;
+        protected boolean working;
+        protected String tel;
+        
+        public String getSchool() {
+            return school;
+        }
+        public void setSchool(String school) {
+            this.school = school;
+        }
+        public boolean isWorking() {
+            return working;
+        }
+        public void setWorking(boolean working) {
+            this.working = working;
+        }
+        public String getTel() {
+            return tel;
+        }
+        public void setTel(String tel) {
+            this.tel = tel;
+        }
+    }
+    
+    static Student[] students = new Student[100];
     
     static int index = 0;
     
@@ -55,9 +80,9 @@ public class App {
             System.out.print("학생 관리> ");
             String command = keyIn.nextLine();
             if (command.equals("list")) {
-                printMembers();
+                printStudents();
             } else if (command.equals("add")) {
-                inputMembers();
+                inputStudents();
             } else if (command.equals("quit")) {
                 break;
             } else {
@@ -90,18 +115,24 @@ public class App {
         }
     }
     
-    static void printMembers() {
-        for (int i = 0; i < index; i++) {
+    static void printStudents() {
+        int count = 0;
+        for (Student s : students) {
+            if (count++ ==  index)
+                break;
             System.out.printf("%s, %s, %s\n",
-                    members[i].getName(),
-                    members[i].getEmail(),
-                    members[i].getPassword());
+                    s.getName(),
+                    s.getEmail(),
+                    s.getPassword(),
+                    s.getSchool(),
+                    s.isWorking(),
+                    s.getTel());
         }
     }
     
-    static void inputMembers() {
+    static void inputStudents() {
         while (true) {
-            Member m = new Member();
+            Student m = new Student();
             
             System.out.println("이름? ");
             m.setName(keyIn.nextLine());
@@ -112,7 +143,16 @@ public class App {
             System.out.println("암호? ");
             m.setPassword(keyIn.nextLine());
             
-            members[index++] = m;
+            System.out.println("최종학력? ");
+            m.setSchool(keyIn.nextLine());
+            
+            System.out.println("재직여부?(true/false) ");
+            m.setWorking(Boolean.parseBoolean(keyIn.nextLine()));
+            
+            System.out.println("전화? ");
+            m.setTel(keyIn.nextLine());
+            
+            students[index++] = m;
             
             System.out.println("계속 하시겠습니까?(Y/n)");
             String answer = keyIn.nextLine();
