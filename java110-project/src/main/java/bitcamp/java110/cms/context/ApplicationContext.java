38a7d24090491c4ab4857374851eee21aec6666a
lee <injin33@naver.com>
2018-09-10 10:08:27 +0900

    애노테이션 적용

--- java110-project/src/main/java/bitcamp/java110/cms/context/ApplicationContext.java
+++ java110-project/src/main/java/bitcamp/java110/cms/context/ApplicationContext.java
@@ -2,10 +2,11 @@ package bitcamp.java110.cms.context;
 
 import java.io.File;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 import java.util.HashMap;
 
 import org.apache.ibatis.io.Resources;
+
+import bitcamp.java110.cms.annotation.Component;
 
 public class ApplicationContext {
     
@@ -59,16 +60,11 @@ public class ApplicationContext {
                     // => 생성자를 가지고 인스턴스를 생성한다.
                     Object instance = constructor.newInstance();
                     
-                    // => 이름으로 인스턴스의 필드 찾는다.
-                    Field field = clazz.getField("name");
+                    // => 클래스에서 Component 애노테이션을 추출한다.
+                    Component anno = clazz.getAnnotation(Component.class);
                     
-                    // => "name" 필드의 값을 꺼낸다.
-                    Object name = field.get(instance);
-                    
-                    System.out.println(clazz.getName() + "==>" + name);
-                    
-                    // => "name" 필드의 값으로 인스턴스를 objPool에 저장한다.
-                    objPool.put((String)name, instance);
+                    // => Component 애노테이션 value 값으로 인스턴스를 objPool에 저장한다.
+                    objPool.put(anno.value(), instance);
                     
                     System.out.println(instance.getClass());
                 } catch (Exception e) {
