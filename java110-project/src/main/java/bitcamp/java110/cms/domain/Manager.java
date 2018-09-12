2da42f6ec1a9338468219be7db41a5819ef84961
lee <injin33@naver.com>
2018-09-12 11:31:47 +0900

    ManagerDao에Serialize 적용

--- java110-project/src/main/java/bitcamp/java110/cms/domain/Manager.java
+++ java110-project/src/main/java/bitcamp/java110/cms/domain/Manager.java
@@ -1,6 +1,8 @@
 package bitcamp.java110.cms.domain;
 
 public class Manager extends Member {
+    private static final long serialVersionUID = 1L;
+    
     protected String tel;
     protected String position;
     
