6c244d1ab22345ae3b7d2cb8f18cedb134a89315
lee <injin33@naver.com>
2018-09-13 17:58:47 +0900

    .

--- java110-project/src/main/java/bitcamp/java110/cms/dao/DaoException.java
+++ java110-project/src/main/java/bitcamp/java110/cms/dao/DaoException.java
@@ -8,7 +8,8 @@ public class DaoException extends RuntimeException {
         super();
     }
 
-    public DaoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    public DaoException(String message, Throwable cause, 
+            boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
 
