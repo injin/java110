515c6071466568179c9545d936237f1b0dcf3782
lee <injin33@naver.com>
2018-09-28 16:40:21 +0900

    포워드 적용

--- java110-project/src/main/java/bitcamp/java110/cms/servlet/student/StudentAddServlet.java
+++ java110-project/src/main/java/bitcamp/java110/cms/servlet/student/StudentAddServlet.java
@@ -1,6 +1,5 @@
 package bitcamp.java110.cms.servlet.student;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -39,28 +38,11 @@ public class StudentAddServlet extends HttpServlet {
             studentDao.insert(s);
             response.sendRedirect("list");
         } catch (Exception e) {
-            e.printStackTrace();
+            request.setAttribute("error", e);
+            request.setAttribute("message", "학생 등록 오류!");
+            request.setAttribute("refresh", "3;url=list");
             
-            // 등록 오류 내용을 출력하고 3초가 경과한 후에 목록 페이지를 요청하도록
-            // "리프래시" 명령을 설정한다.
-            // => 응답할 때 응답 헤더로 리프래시에 대한 명령을 웹브라우저에게 전달한다.
-            response.setHeader("Refresh", "3;url=list");
-            
-            response.setContentType("text/html;charset=UTF-8");
-            PrintWriter out = response.getWriter();
-            
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<meta charset='UTF-8'>");
-            out.println("<title>학생 관리</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>학생 등록 오류</h1>");
-            out.printf("<p>%s</p>\n", e.getMessage());
-            out.println("<p>잠시 기다리면 목록 페이지로 자동으로 이동합니다.</p>");
-            out.println("</body>");
-            out.println("</html>");
+            request.getRequestDispatcher("/error").forward(request, response);
         }
         
         
