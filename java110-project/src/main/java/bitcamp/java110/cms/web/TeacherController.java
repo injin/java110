96379ceab9f8aa2e2936b535463b551052575565
lee <injin33@naver.com>
2018-10-23 12:26:44 +0900

    Spring WebMVC 적용

--- java110-project/src/main/java/bitcamp/java110/cms/web/TeacherController.java
+++ java110-project/src/main/java/bitcamp/java110/cms/web/TeacherController.java
@@ -8,14 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import bitcamp.java110.cms.domain.Teacher;
-import bitcamp.java110.cms.mvc.RequestMapping;
-import bitcamp.java110.cms.mvc.RequestParam;
 import bitcamp.java110.cms.service.TeacherService;
 
-@Component
+@Controller
 public class TeacherController {
     
     @Autowired
@@ -41,7 +41,7 @@ public class TeacherController {
     
     @RequestMapping("/teacher/detail")
     public String detail(
-            @RequestParam("no") int no,
+            int no,
             Map<String, Object> map) {
         
         Teacher t = teacherService.get(no);
@@ -72,8 +72,7 @@ public class TeacherController {
     }
     
     @RequestMapping("/teacher/delete")
-    public String delete(
-            @RequestParam("no") int no) throws Exception {
+    public String delete(int no) throws Exception {
         
         teacherService.delete(no);
         return "redirect:list";
